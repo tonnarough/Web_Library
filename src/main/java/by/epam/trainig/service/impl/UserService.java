90d29372184b50aa358036944764637367a9d9d2
tonnarough <yatsevich57@mail.ru>
2022-01-14 11:14:34 +0300

    created simplest registration method

--- src/main/java/by/epam/trainig/service/impl/UserService.java
+++ src/main/java/by/epam/trainig/service/impl/UserService.java
@@ -1,9 +1,12 @@
 package by.epam.trainig.service.impl;
 
 import by.epam.trainig.dao.impl.MethodUserDAO;
+import by.epam.trainig.dao.impl.MethodUserDetailDAO;
 import by.epam.trainig.entity.user.User;
+import by.epam.trainig.entity.user.UserDetail;
 import by.epam.trainig.service.EntityService;
 
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +14,7 @@ import java.util.Optional;
 public class UserService implements EntityService<User> {
 
     private final MethodUserDAO methodUserDAO = new MethodUserDAO();
+    private final MethodUserDetailDAO methodUserDetailDAO = new MethodUserDetailDAO();
 
     @Override
     public List<User> findAll() throws SQLException {
@@ -29,10 +33,24 @@ public class UserService implements EntityService<User> {
         if (user.isPresent()) {
             if (user.get().getPassword().equals(password)) {
                 return user;
-            }else{
+            } else {
                 return Optional.empty();
             }
         }
-            return Optional.empty();
+        return Optional.empty();
+    }
+
+    public void registration(String login, String password, String lastName, String firstName,
+                             String fatherName, String email, String mobile, Date birthday) {
+        User user = new User(login, password);
+        UserDetail userDetail = new UserDetail(lastName, firstName, fatherName, email, mobile, birthday);
+
+        methodUserDAO.create(user);
+        methodUserDetailDAO.create(userDetail);
+    }
+
+    public boolean isExists(String login){
+        Optional<User> user = methodUserDAO.findByLogin(login);
+        return user.isPresent();
     }
 }
