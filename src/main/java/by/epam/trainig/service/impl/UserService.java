4f680ba1c153a5db5c6dcbe54ad351bf085e00e0
tonnarough <yatsevich57@mail.ru>
2022-01-17 10:57:17 +0300

    upd authenticate with hashed password

--- src/main/java/by/epam/trainig/service/impl/UserService.java
+++ src/main/java/by/epam/trainig/service/impl/UserService.java
@@ -1,11 +1,13 @@
 package by.epam.trainig.service.impl;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import by.epam.trainig.dao.impl.MethodUserDAO;
 import by.epam.trainig.dao.impl.MethodUserDetailDAO;
 import by.epam.trainig.entity.user.User;
 import by.epam.trainig.entity.user.UserDetail;
 import by.epam.trainig.service.EntityService;
 
+import java.nio.charset.StandardCharsets;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.List;
@@ -15,6 +17,9 @@ public class UserService implements EntityService<User> {
 
     private final MethodUserDAO methodUserDAO = new MethodUserDAO();
     private final MethodUserDetailDAO methodUserDetailDAO = new MethodUserDetailDAO();
+
+    private final BCrypt.Hasher hasher = BCrypt.withDefaults();
+    private final BCrypt.Verifyer verifyer = BCrypt.verifyer();
 
     @Override
     public List<User> findAll() throws SQLException {
@@ -28,14 +33,20 @@ public class UserService implements EntityService<User> {
 
     public Optional<User> authenticate(String login, String password) {
 
+        if (login == null || password == null) {
+            return Optional.empty();
+        }
+
         final Optional<User> user = methodUserDAO.findByLogin(login);
+        final byte[] enteredPassword = password.getBytes(StandardCharsets.UTF_8);
 
         if (user.isPresent()) {
-            if (user.get().getPassword().equals(password)) {
-                return user;
-            } else {
-                return Optional.empty();
-            }
+            final byte[] hashedPassword = user.get()
+                    .getPassword()
+                    .getBytes(StandardCharsets.UTF_8);
+            return verifyer.verify(enteredPassword, hashedPassword).verified
+                    ? user
+                    : Optional.empty();
         }
         return Optional.empty();
     }
@@ -49,7 +60,7 @@ public class UserService implements EntityService<User> {
         methodUserDetailDAO.create(userDetail);
     }
 
-    public boolean isExists(String login){
+    public boolean isExists(String login) {
         Optional<User> user = methodUserDAO.findByLogin(login);
         return user.isPresent();
     }
