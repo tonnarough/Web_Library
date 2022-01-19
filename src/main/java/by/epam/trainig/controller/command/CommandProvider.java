653329030a6797edf1a0171ddb30ee3d2570b821
tonnarough <yatsevich57@mail.ru>
2022-01-20 01:35:34 +0300

    changed CommandProvider from class to enum

--- src/main/java/by/epam/trainig/controller/command/CommandProvider.java
+++ src/main/java/by/epam/trainig/controller/command/CommandProvider.java
@@ -3,25 +3,28 @@ package by.epam.trainig.controller.command;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class CommandProvider{
+public enum CommandProvider {
+    SIGN_IN(new LoginCommand(), "sign_in"),
+    REGISTRATION(new RegistrationCommand(), "registration"),
+    GO_TO_LOGIN_PAGE(new GoToLoginPageCommand(), "GO_TO_LOGIN_PAGE"),
+    GO_TO_REGISTRATION_PAGE(new GoToRegistrationCommand(), "GO_TO_REGISTRATION_PAGE"),
+    GO_TO_MAIN_AUTH_PAGE(new GoToMainAuthPageCommand(), "go_to_main_auth_page"),
+    DEFAULT(new GoToMainAuthPageCommand(), "go_to_main_auth_page");
 
-    private final String SIGN_IN = "sign_in";
-    private final String REGISTRATION = "registration";
-    private final String GO_TO_LOGIN_PAGE = "GO_TO_LOGIN_PAGE";
-    private final String GO_TO_REGISTRATION_PAGE = "GO_TO_REGISTRATION_PAGE";
-    private final String GO_TO_MAIN_AUTH_PAGE = "go_to_main_auth_page";
+    private final String path;
+    private final Command command;
 
-    private final Map<String, Command> commands = new HashMap<>();
-
-    public CommandProvider() {
-    commands.put(SIGN_IN, new LoginCommand());
-    commands.put(REGISTRATION, new RegistrationCommand());
-    commands.put(GO_TO_REGISTRATION_PAGE, new GoToRegistrationCommand());
-    commands.put(GO_TO_LOGIN_PAGE, new GoToLoginPageCommand());
-    commands.put(GO_TO_MAIN_AUTH_PAGE, new GoToMainAuthPageCommand());
+    CommandProvider(Command command, String path) {
+        this.command = command;
+        this.path = path;
     }
 
-    public Command getCommand(String commandName){
-        return commands.get(commandName);
+    public static Command getCommand(String commandName) {
+        for(CommandProvider constant : values()){
+            if(constant.path.equalsIgnoreCase(commandName)){
+                return constant.command;
+            }
+        }
+        return DEFAULT.command;
     }
 }
