77a6ccba59abc8c65d8dc5af5a19fc9ead31c022
tonnarough <yatsevich57@mail.ru>
2022-01-20 03:26:25 +0300

    changed from class to enum

--- src/main/java/by/epam/trainig/controller/command/CommandProvider.java
+++ src/main/java/by/epam/trainig/controller/command/CommandProvider.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum CommandProvider {
-    SIGN_IN(new LoginCommand(), "sign_in"),
-    REGISTRATION(new RegistrationCommand(), "registration"),
-    GO_TO_LOGIN_PAGE(new GoToLoginPageCommand(), "GO_TO_LOGIN_PAGE"),
-    GO_TO_REGISTRATION_PAGE(new GoToRegistrationCommand(), "GO_TO_REGISTRATION_PAGE"),
-    GO_TO_MAIN_AUTH_PAGE(new GoToMainAuthPageCommand(), "go_to_main_auth_page"),
-    DEFAULT(new GoToMainAuthPageCommand(), "go_to_main_auth_page");
+    SIGN_IN(LoginCommand.INSTANCE, "sign_in"),
+    REGISTRATION(RegistrationCommand.INSTANCE, "registration"),
+    GO_TO_LOGIN_PAGE(GoToLoginPageCommand.INSTANCE, "go_to_login_page"),
+    GO_TO_REGISTRATION_PAGE(GoToRegistrationCommand.INSTANCE, "go_to_registration_page"),
+    GO_TO_MAIN_AUTH_PAGE(GoToMainAuthPageCommand.INSTANCE, "go_to_main_auth_page"),
+    DEFAULT(GoToMainAuthPageCommand.INSTANCE, "go_to_main_auth_page");
 
     private final String path;
     private final Command command;
