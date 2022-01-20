0dcdd8b7ff7e261ffb15c4d817b1a1959c8431ce
tonnarough <yatsevich57@mail.ru>
2022-01-20 03:42:21 +0300

    created interface implementation (PropertyContext)

---
+++ src/main/java/by/epam/trainig/controller/PropertyContextImpl.java
@@ -0,0 +1,32 @@
+package by.epam.trainig.controller;
+
+public final class PropertyContextImpl implements PropertyContext {
+
+    private static final String PAGE_START_WITH_FOR_FORWARD = "page.";
+    private static final String PAGE_START_WITH_FOR_REDIRECT = "go_to";
+    private static final String URL = "controller?command=";
+
+    private PropertyContextImpl() {
+    }
+
+    public static PropertyContextImpl getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    @Override
+    public String get(String name) {
+        if (name.startsWith(PAGE_START_WITH_FOR_FORWARD)) {
+            return PagePath.of(name.substring(5)).getPath();
+        } else if (name.startsWith(PAGE_START_WITH_FOR_REDIRECT)) {
+            return URL + name;
+        }
+        return null;
+        //TODO exception
+    }
+
+    private static class Holder {
+        public static final PropertyContextImpl INSTANCE = new PropertyContextImpl();
+    }
+
+}
+
