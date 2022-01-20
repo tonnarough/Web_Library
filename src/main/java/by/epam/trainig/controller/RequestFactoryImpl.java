98fb47f9f89dcee62c2c3bf41c16020a3db6e74a
tonnarough <yatsevich57@mail.ru>
2022-01-20 03:39:08 +0300

    created interface implemetation (RequestFactory)

---
+++ src/main/java/by/epam/trainig/controller/RequestFactoryImpl.java
@@ -0,0 +1,31 @@
+package by.epam.trainig.controller;
+
+import by.epam.trainig.controller.command.CommandRequest;
+import by.epam.trainig.controller.command.CommandResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public enum RequestFactoryImpl implements RequestFactory{
+    INSTANCE;
+
+    private final Map<String, CommandResponse> forwardResponseCache = new ConcurrentHashMap<>();
+    private final Map<String, CommandResponse> redirectResponseCache = new ConcurrentHashMap<>();
+
+    @Override
+    public CommandRequest createRequest(HttpServletRequest request) {
+        return new WrappingCommandRequest(request);
+    }
+
+    @Override
+    public CommandResponse createForwardResponse(String path) {
+        return forwardResponseCache.computeIfAbsent(path, WrappingCommandResponse::new);
+    }
+
+    @Override
+    public CommandResponse createRedirectResponse(String path) {
+        return redirectResponseCache.computeIfAbsent(path, p -> new WrappingCommandResponse(true,p));
+    }
+
+}
