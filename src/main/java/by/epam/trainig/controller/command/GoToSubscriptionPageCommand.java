e7184bfb342311040283ad67762eac36364017a6
tonnarough <yatsevich57@mail.ru>
2022-01-22 00:40:11 +0300

    worked with subscription

--- src/main/java/by/epam/trainig/controller/command/GoToSubscriptionPageCommand.java
+++ src/main/java/by/epam/trainig/controller/command/GoToSubscriptionPageCommand.java
@@ -2,22 +2,33 @@ package by.epam.trainig.controller.command;
 
 import by.epam.trainig.controller.PropertyContext;
 import by.epam.trainig.controller.RequestFactory;
+import by.epam.trainig.entity.user.SubscriptionType;
+import by.epam.trainig.service.SubscriptionService;
 
-public enum GoToSubscriptionPageCommand implements Command{
-    INSTANCE(PropertyContext.getInstance(), RequestFactory.getInstance());
+import java.util.List;
+
+public enum GoToSubscriptionPageCommand implements Command {
+    INSTANCE(SubscriptionService.getInstance(), PropertyContext.getInstance(), RequestFactory.getInstance());
 
     private static final String SUBSCRIPTION_PAGE = "page.subscription";
+    private static final String SUBSCRIPTION_PARAMETER = "subscriptionTypes";
 
+    private final SubscriptionService subscriptionService;
     private final PropertyContext propertyContext;
     private final RequestFactory requestFactory;
 
-    GoToSubscriptionPageCommand(PropertyContext propertyContext, RequestFactory requestFactory) {
+    GoToSubscriptionPageCommand(SubscriptionService subscriptionService, PropertyContext propertyContext, RequestFactory requestFactory) {
+        this.subscriptionService = subscriptionService;
         this.propertyContext = propertyContext;
         this.requestFactory = requestFactory;
     }
 
     @Override
     public CommandResponse execute(CommandRequest request) {
+
+        final List<SubscriptionType> subscriptionTypes = subscriptionService.findAllTypes();
+
+        request.addAttributeToJsp(SUBSCRIPTION_PARAMETER, subscriptionTypes);
 
         return requestFactory.createForwardResponse(propertyContext.get(SUBSCRIPTION_PAGE));
     }
--- src/main/java/by/epam/trainig/controller/command/SubscriptionCommand.java
+++ src/main/java/by/epam/trainig/controller/command/SubscriptionCommand.java
@@ -2,26 +2,20 @@ package by.epam.trainig.controller.command;
 
 import by.epam.trainig.controller.PropertyContext;
 import by.epam.trainig.controller.RequestFactory;
-import by.epam.trainig.entity.user.User;
-import by.epam.trainig.service.UserService;
-
-import java.util.Optional;
+import by.epam.trainig.service.SubscriptionService;
 
 public enum SubscriptionCommand implements Command{
-    INSTANCE(UserService.getInstance(), PropertyContext.getInstance(), RequestFactory.getInstance());
+    INSTANCE(SubscriptionService.getInstance(), PropertyContext.getInstance(), RequestFactory.getInstance());
 
-    private static final String SUBSCRIPTION_PAGE = "go_to_subscription_page";
     private static final String MAIN_AUTH_PAGE = "go_to_main_auth_page";
 
-    private static final String LOGIN_REQUEST_PARAMETR_NAME = "login";
-    private static final String PASSWORD_REQUEST_PARAMETR_NAME = "password";
 
-    private final UserService userService;
+    private final SubscriptionService subscriptionService;
     private final PropertyContext propertyContext;
     private final RequestFactory requestFactory;
 
-    SubscriptionCommand(UserService userService, PropertyContext propertyContext, RequestFactory requestFactory) {
-        this.userService = userService;
+    SubscriptionCommand(SubscriptionService subscriptionService, PropertyContext propertyContext, RequestFactory requestFactory) {
+        this.subscriptionService = subscriptionService;
         this.propertyContext = propertyContext;
         this.requestFactory = requestFactory;
     }
@@ -29,15 +23,6 @@ public enum SubscriptionCommand implements Command{
     @Override
     public CommandResponse execute(CommandRequest request) {
 
-        final String login = request.getParameter(LOGIN_REQUEST_PARAMETR_NAME);
-        final String password = request.getParameter(PASSWORD_REQUEST_PARAMETR_NAME);
-        final Optional<User> user = userService.authenticate(login, password);
-
-        if (user.isPresent()) {
-            return requestFactory.createRedirectResponse(propertyContext.get(MAIN_AUTH_PAGE));
-        }
-
-        return requestFactory.createRedirectResponse(propertyContext.get(SUBSCRIPTION_PAGE));
-
+        return requestFactory.createRedirectResponse(propertyContext.get(MAIN_AUTH_PAGE));
     }
 }
