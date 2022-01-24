1835f9d6b381bf4447d9827634f3358d4349b9c1
tonnarough <yatsevich57@mail.ru>
2022-01-25 00:01:35 +0300

    deleted driver

--- src/main/java/by/epam/trainig/dao/connectionpool/DataBaseProperties.java
+++ src/main/java/by/epam/trainig/dao/connectionpool/DataBaseProperties.java
@@ -2,7 +2,6 @@ package by.epam.trainig.dao.connectionpool;
 
 public final class DataBaseProperties {
 
-    public static final String DRIVER = "driver";
     public static final String URL = "url";
     public static final String USER = "username";
     public static final String PASSWORD = "password";
