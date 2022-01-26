2d3e7acf19de05214e37b2349c08c8ca44b5ea6d
tonnarough <yatsevich57@mail.ru>
2022-01-26 12:53:14 +0300

    created SetLocaleCommand

--- src/main/java/by/epam/trainig/controller/command/GoToMainUnauthPageCommand.java
+++ src/main/java/by/epam/trainig/controller/command/GoToMainUnauthPageCommand.java
@@ -1,26 +1,20 @@
 package by.epam.trainig.controller.command;
 
-import by.epam.trainig.controller.PagePath;
 import by.epam.trainig.controller.PropertyContext;
 import by.epam.trainig.controller.RequestFactory;
-import by.epam.trainig.service.impl.UserServiceImpl;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.SQLException;
-
-public enum GoToMainAuthPageCommand implements Command {
+public enum GoToMainUnauthPageCommand implements Command {
     INSTANCE(PropertyContext.getInstance(), RequestFactory.getInstance());
 
-    private static final String MAIN_AUTH_PAGE = "page.main_auth";
+    private static final String MAIN_UNAUTH_PAGE = "page.main_unauth";
+    private static final String URL = "url";
+    private static final String PARAMETER_NAME = "?command=";
+    private static final String PARAMETER_FROM_REQUEST = "command";
 
     private final PropertyContext propertyContext;
     private final RequestFactory requestFactory;
 
-    GoToMainAuthPageCommand(PropertyContext propertyContext, RequestFactory requestFactory) {
+    GoToMainUnauthPageCommand(PropertyContext propertyContext, RequestFactory requestFactory) {
         this.propertyContext = propertyContext;
         this.requestFactory = requestFactory;
     }
@@ -28,6 +22,10 @@ public enum GoToMainAuthPageCommand implements Command {
     @Override
     public CommandResponse execute(CommandRequest request) {
 
-        return requestFactory.createForwardResponse(propertyContext.get(MAIN_AUTH_PAGE));
+        final String url = request.getRequestURL() + PARAMETER_NAME + request.getParameter(PARAMETER_FROM_REQUEST);
+
+        request.addToSession(URL, url);
+
+        return requestFactory.createForwardResponse(propertyContext.get(MAIN_UNAUTH_PAGE));
     }
 }
