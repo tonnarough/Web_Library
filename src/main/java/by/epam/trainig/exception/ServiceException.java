285dc7730e05b781aecd75f79f136a019d508a3b
tonnarough <yatsevich57@mail.ru>
2022-01-27 23:31:18 +0300

    created Service & DAO exception

---
+++ src/main/java/by/epam/trainig/exception/ServiceException.java
@@ -0,0 +1,17 @@
+package by.epam.trainig.exception;
+
+public class ServiceException extends Exception{
+
+    public ServiceException() {
+        super();
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
