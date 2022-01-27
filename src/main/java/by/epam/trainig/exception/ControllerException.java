11ae135105279720b307fd7cdfab9cb316f30288
tonnarough <yatsevich57@mail.ru>
2022-01-28 01:34:47 +0300

    created ControllerException

--- src/main/java/by/epam/trainig/exception/ControllerException.java
+++ src/main/java/by/epam/trainig/exception/ControllerException.java
@@ -1,2 +1,21 @@
-package by.epam.trainig.exception;public class ControllerException {
+package by.epam.trainig.exception;
+
+public class ControllerException extends Exception{
+
+    public ControllerException() {
+        super();
+    }
+
+    public ControllerException(String message) {
+        super(message);
+    }
+
+    public ControllerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ControllerException(Throwable cause) {
+        super(cause);
+    }
+
 }
