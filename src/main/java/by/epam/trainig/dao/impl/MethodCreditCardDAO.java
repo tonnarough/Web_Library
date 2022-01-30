d65c346e5636db5e08763272a45ec5115825851d
tonnarough <yatsevich57@mail.ru>
2022-01-30 08:01:36 +0300

    created service & DAO for Credit card

--- src/main/java/by/epam/trainig/dao/impl/MethodCreditCardDAO.java
+++ src/main/java/by/epam/trainig/dao/impl/MethodCreditCardDAO.java
@@ -5,8 +5,8 @@ import by.epam.trainig.context.DatabaseEntityContext;
 import by.epam.trainig.dao.BankAccountDAO;
 import by.epam.trainig.dao.connectionpool.ConnectionPool;
 import by.epam.trainig.dao.queryoperation.QueryOperation;
-import by.epam.trainig.entity.user.BankAccount;
 import by.epam.trainig.entity.user.CreditCard;
+import by.epam.trainig.entity.user.User;
 import by.epam.trainig.exception.DAOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,10 +23,7 @@ public enum MethodBankAccountDAO implements BankAccountDAO {
 
     private final QueryOperation queryOperation = QueryOperation.getInstance();
 
-    private final Class<BankAccount> bankAccountClass = BankAccount.class;
-    private final Table tableBankAccountClass = bankAccountClass.getAnnotation(Table.class);
-    private final List<String> bankAccountColumnNames = DatabaseEntityContext
-            .getDatabaseEntityContext().getDatabaseContext(tableBankAccountClass.name());
+    private static final String ADDED_IN_BANK_ACCOUNT_TABLE = "INSERT INTO bank_account (user_id,credit_card_id) VALUES (?,?)";
 
     private final Class<CreditCard> creditCardClass = CreditCard.class;
     private final Table tableCreditCardClass = creditCardClass.getAnnotation(Table.class);
@@ -39,7 +36,7 @@ public enum MethodBankAccountDAO implements BankAccountDAO {
     }
 
     @Override
-    public List<BankAccount> findAll() throws DAOException {
+    public List<CreditCard> findAll() throws DAOException {
 
         return null;
 
@@ -51,12 +48,12 @@ public enum MethodBankAccountDAO implements BankAccountDAO {
     }
 
     @Override
-    public void create(BankAccount entity) {
+    public void create(CreditCard entity) {
 
     }
 
     @Override
-    public void create(BankAccount bankAccount, CreditCard creditCard) throws DAOException {
+    public void create(User user, CreditCard creditCard) throws DAOException {
 
         Connection connection = ConnectionPool.getConnectionPool().getConnection();
 
@@ -71,9 +68,7 @@ public enum MethodBankAccountDAO implements BankAccountDAO {
             Optional<CreditCard> creditCardFromDB = queryOperation.findBy(tableCreditCardClass, creditCardColumnNames.get(1),
                     creditCard.getCreditCardNumber(), CreditCard.class);
 
-            creditCardFromDB.ifPresent(detail -> bankAccount.setCreditCardId(detail.getId()));
-
-            queryOperation.create(bankAccountColumnNames, tableBankAccountClass, bankAccount, BankAccount.class, connection);
+            queryOperation.create(ADDED_IN_BANK_ACCOUNT_TABLE, user.getId(), creditCardFromDB.get().getId(), connection);
 
             connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 
@@ -92,7 +87,7 @@ public enum MethodBankAccountDAO implements BankAccountDAO {
     }
 
     @Override
-    public Optional<BankAccount> findBy(String columnName, Object value) {
+    public Optional<CreditCard> findBy(String columnName, Object value) {
 
         return Optional.empty();
 
