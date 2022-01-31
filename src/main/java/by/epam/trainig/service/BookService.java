aa81a453317bd5fcffde7d5d7802142d394e1a04
tonnarough <yatsevich57@mail.ru>
2022-01-31 04:29:54 +0300

    added find methods

--- src/main/java/by/epam/trainig/service/BookService.java
+++ src/main/java/by/epam/trainig/service/BookService.java
@@ -1,9 +1,22 @@
 package by.epam.trainig.service;
 
+import by.epam.trainig.entity.book.Author;
 import by.epam.trainig.entity.book.Book;
+import by.epam.trainig.entity.book.Genre;
+import by.epam.trainig.entity.book.PublishingHouse;
 import by.epam.trainig.service.impl.BookServiceImpl;
 
+import java.util.List;
+
 public interface BookService extends EntityService<Book>{
+
+    List<Book> findByAuthor(Author author);
+
+    List<Book> findByPublishingHouse(PublishingHouse publishingHouse);
+
+    List<Book> findByGenre(Genre genre);
+
+    List<Book> findAllBook();
 
     static BookServiceImpl getInstance(){
         return BookServiceImpl.INSTANCE;
--- src/main/java/by/epam/trainig/service/impl/BookServiceImpl.java
+++ src/main/java/by/epam/trainig/service/impl/BookServiceImpl.java
@@ -1,21 +1,66 @@
 package by.epam.trainig.service.impl;
 
+import by.epam.trainig.dao.BookDAO;
+import by.epam.trainig.entity.book.Author;
 import by.epam.trainig.entity.book.Book;
+import by.epam.trainig.entity.book.Genre;
+import by.epam.trainig.entity.book.PublishingHouse;
+import by.epam.trainig.exception.DAOException;
 import by.epam.trainig.exception.ServiceException;
 import by.epam.trainig.service.BookService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public enum BookServiceImpl implements BookService {
-    INSTANCE;
+    INSTANCE(BookDAO.getInstance());
+
+    private static final Logger logger = LogManager.getLogger(BookServiceImpl.class);
+
+    private final BookDAO bookDAO;
+
+    BookServiceImpl(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
 
     @Override
     public List<Book> findAll() throws ServiceException {
-        return null;
+
+        try {
+
+            return bookDAO.findAll();
+
+        } catch (DAOException e) {
+
+            logger.error("Failed finding of all books", e);
+            throw new ServiceException(e);
+        }
+
     }
 
     @Override
     public void create(Book entity) {
 
+    }
+
+    @Override
+    public List<Book> findByAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findByPublishingHouse(PublishingHouse publishingHouse) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findByGenre(Genre genre) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAllBook() {
+        return null;
     }
 }
