fabf87dbe98c8b1ab866857aee7610a1ac2fd378
tonnarough <yatsevich57@mail.ru>
2022-02-13 03:14:41 +0300

    added new exception

--- src/main/java/by/epam/trainig/exception/EntityExtractionFailedException.java
+++ src/main/java/by/epam/trainig/exception/EntityExtractionFailedException.java
@@ -1,2 +1,19 @@
-package by.epam.trainig.exception;public class EntityExtractionFailedException {
+package by.epam.trainig.exception;
+
+public class EntityExtractionFailedException extends Exception{
+
+    public EntityExtractionFailedException() {
+    }
+
+    public EntityExtractionFailedException(String message) {
+        super(message);
+    }
+
+    public EntityExtractionFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityExtractionFailedException(Throwable cause) {
+        super(cause);
+    }
 }
